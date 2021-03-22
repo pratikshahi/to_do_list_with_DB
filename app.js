d1f933ce0515b84c89543f8dcf75d271721aeba4
Pratik Shahi <shahipr0@gmail.com>
2021-03-22 21:30:54 +0530

    storing user added list to db and displaying back

--- app.js
+++ app.js
@@ -3,61 +3,67 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
-const mongoose = require("mongoose");                 //requiring mongoose   
+const mongoose = require("mongoose"); //requiring mongoose
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });       //creating and connecting to DB in localhost
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}); //creating and connecting to DB in localhost
 
-const itemSchema = {                                          //creating new moongose schema
-  name: String
+const itemSchema = {
+  //creating new moongose schema
+  name: String,
 };
 
-const Item = mongoose.model("Item", itemSchema);                     //creating mongoose model
+const Item = mongoose.model("Item", itemSchema); //creating mongoose model
 
-const item1 = new Item({                                           //CREATing 2 default data in items collection
-  name: "Wellcome to to do list"
+const item1 = new Item({
+  //CREATing 2 default data in items collection
+  name: "Wellcome to to do list",
 });
 const item2 = new Item({
-  name: "Hit + after you entered your list"
+  name: "Hit + after you entered your list",
 });
 
 const defaultItems = [item1, item2];
 
-Item.insertMany(defaultItems, function (err) {                         //insert many item to db
-  if (err) {
-    console.log("error inside insertMany" + err);
-  }
-  else {
-    console.log("sucessfulliy saved defaultitem to db");
-  }
-});
-
-
 app.get("/", function (req, res) {
+  Item.find(function (err, foundItems) {
+    //reading data from db
+
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        //insert many item to db
+        if (err) {
+          console.log("error inside insertMany" + err);
+        } else {
+          console.log("sucessfulliy saved defaultitem to db");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: day, newListItems: foundItems });
+    }
+  });
 
   const day = date.getDate();
-
-  res.render("list", { listTitle: day, newListItems: items });
-
 });
 
 app.post("/", function (req, res) {
-
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const itemName = req.body.newItem;
+  //passing user entered list to db
+  const item = new Item({
+    name: itemName,
+  });
+  item.save();
+  res.redirect("/");
 });
 
 app.get("/work", function (req, res) {
